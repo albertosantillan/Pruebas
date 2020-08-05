0c022a8da349b7f175dabb06fb190443dc076487
Martin Paredes <alberto_santillan.ind@hotmail.com>
2020-08-05 14:42:39 -0500

    etiquetas css

--- foreach.php
+++ foreach.php
@@ -1,17 +1,40 @@
+<html>
+<link rel="stylesheet" href='https://stackpath.bootstrapcdn.com/bootswatch/4.5.0/darkly/bootstrap.min.css'>
 <?php
 $persona = array(
     'datos' => array(
       'nombre' => 'Martin',
       'pais' => 'Mexico',
-      'profesion' => 'Ing. industrial'
+      'profesion' => 'Ing. industrial',
+      'status' => 'inactivoo'
     ),
     'Lenguajes' => array(
       'front_end' => array('css','html','javascript'),
       'Back_end' => array('PHP','mysql','python')  
         )
   );
- foreach($persona ['datos'] as $person):
- echo $person;
+  echo $persona['datos']['pais'].'<br>';
+switch ($persona['datos']['status']) {
+  case 'activo':
+    $tag_status = 'bg-warning';
+    break;
+    case 'inactivo':
+      $tag_status = 'bg-danger';
+      break;
+  default:
+  $tag_status = 'bg-primary';
+    break;
+}
+echo '<ul class="list-group w-25 m-4">';
+ foreach($persona ['datos'] as $key => $person):
+ echo "<li class='list-group-item $tag_status'>$key - $person</li>";
 endforeach;
-
+echo '</ul> <br>';
+echo '<select class="m-4 custom-select w-25">';
+foreach ($persona['Lenguajes'] ['front_end'] as $key => $value) {
+  echo "<option> $value </option>";
+}
+echo '</select><br>'; 
+echo '<button class="m-4 btn btn-info btn-sm btn-block w-25"> continuar </button>';
 ?>
+<html> 
