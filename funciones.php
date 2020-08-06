4b8ddc201c44481ab60f4182a62b5b8a4ad333d4
Martin Alberto Santillan <alberto_santillan.ind@hotmail.com>
2020-08-05 23:51:16 -0500

    cambios

---
+++ funciones.php
@@ -0,0 +1,15 @@
+<html>
+<link rel="stylesheet" href='https://stackpath.bootstrapcdn.com/bootswatch/4.5.0/darkly/bootstrap.min.css'>
+<h2>Agenda telefónica</h2>
+<?php
+function usuario($nombre,$tel){
+  echo $nombre . "<br>";
+  echo $tel . "<br>";
+  echo "<hr>";
+} 
+
+usuario('Martin','1234-5678-00');
+usuario('Alberto','5678-9012-00');
+
+?>
+<html> 
