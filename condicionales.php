638b04bfff84f68e3fcc7d9559fdd1c43ac64695
Martin Alberto Santillan <alberto_santillan.ind@hotmail.com>
2020-08-06 21:26:16 -0500

    cambios

--- condicionales.php
+++ condicionales.php
@@ -1,4 +1,5 @@
 <?php
+//Validando # caracteres contraseña
 $contrasena = "12345678";
 $total_letras = strlen($contrasena);
 if ($total_letras !== 8) {
@@ -7,4 +8,28 @@ if ($total_letras !== 8) {
 else {
      echo "Tu contraseña se creó correctamente";
 }
+//Imprimiendo #s 1 al 20
+for ($f = 1; $f <= 20; $f++) {
+  echo $f;
+  echo "<br>";
+}
+//Imprimiento aleatorios 1 al 20, iniciando en 1
+$valor = rand(1, 20);
+$inicio = 1;
+while ($inicio <= $valor) {
+  echo $inicio;
+  echo "<br>";
+  $inicio++;
+}
+
+  //Números de los días del 1 a la fecha actual;
+  $dia = date("d");
+  $inicio = 1;
+  while ($inicio < $dia) {
+    echo $inicio . "<br>";
+    $inicio++;
+  }
+
+
+
 ?>
